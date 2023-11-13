2e613c54666d390060314f02d8037a669df3132d
emilioriver <143732286+andreajimenez203@users.noreply.github.com>
2023-11-13 12:46:31 -0600

    ajustes

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,12 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Future<List<Gif>> _listadoGifs;
+  late Future<List<Gif>> _listadoGifs;
 
   Future<List<Gif>> _getGifs() async {
     final response = await http.get(
-        "https://api.giphy.com/v1/gifs/trending?api_key=S2a9zt1K87kBuM1hVqvFX0oOeMN7QYiZ&limit=10&offset=0&rating=g&bundle=messaging_non_clips");
+    Uri.parse('https://api.giphy.com/v1/gifs/trending?api_key=S2a9zt1K87kBuM1hVqvFX0oOeMN7QYiZ&limit=10&offset=0&rating=g&bundle=messaging_non_clips')
+);
 
     if (response.statusCode == 200) {
 
@@ -25,15 +26,22 @@ class _MyAppState extends State<MyApp> {
 
       List<Gif> gifs = [];
 
-      for (var item in json Data["data"]) {
-        gifs.add(
-          Gif(item["title"], item["images"]["fixed_height"]["url"]));
-      }
+    for (var item in jsonData["data"]) {
+  gifs.add(
+    Gif(
+      name: item["title"] ?? "", // Utilizamos ?? para manejar casos en los que el título puede ser nulo
+      url: item["images"]["fixed_height"]["url"] ?? "", // Manejamos el caso en que la URL pueda ser nula
+    ),
+  );
+}
+
       return gifs;
 
-    } else {
-      throw Exception("Falló la conexión");
-    }
+   } else {
+  print("Error en la respuesta: ${response.statusCode}");
+  print("Cuerpo de la respuesta: ${response.body}");
+  throw Exception("Falló la conexión");
+}
   }
 
   @override
@@ -52,7 +60,7 @@ class _MyAppState extends State<MyApp> {
         body:FutureBuilder(future: _listadoGifs, builder: (context , snapshot){
           if (snapshot.hasData){
             return ListView(
-              children: _ListGifs(snapshot.data),
+              children: _listGifs(snapshot.data),
             );
           }else if (snapshot.hasError){
             print(snapshot.error);
@@ -68,24 +76,30 @@ class _MyAppState extends State<MyApp> {
     );
   }
 
-List<Widget> _listGifs(List<Gif> data){
+List<Widget> _listGifs(List<Gif>? data) {
+  if (data == null) {
+    // Manejar el caso de datos nulos según sea necesario
+    return [];
+  }
+
   List<Widget> gifs = [];
 
-for (var gif in data) {
-  gifs.add(
-    Card(child: Column(
-      children: [
-        Image.network(gif.url),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(gif.name),
+  for (var gif in data) {
+    gifs.add(
+      Card(
+        child: Column(
+          children: [
+            Image.network(gif.url),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(gif.name),
+            ),
+          ],
         ),
-      ],
-    ))
-  );
-}
-return gifs;
-
+      ),
+    );
+  }
+  return gifs;
 }
 
 
--- lib/models/Gif.dart
+++ lib/models/Gif.dart
@@ -2,5 +2,5 @@ class Gif {
   String name;
   String url;
 
-  Gif(item,{required this.name, required this.url});
+  Gif({required this.name, required this.url});
 }
